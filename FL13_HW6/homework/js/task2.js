16eb6157845e64c9133047a56ed7631fa74eaf67
Sk3ll <sk3llyar@gmail.com>
2020-04-22 19:39:46 +0300

    hw6t2

--- FL13_HW6/homework/js/task2.js
+++ FL13_HW6/homework/js/task2.js
@@ -1,1 +1,23 @@
-// Your code goes here
+'use strict';
+
+let message;
+let word = [];
+let half = 2;
+message = prompt('Input word', '');
+
+
+if (message === null || !isNaN(message)){
+    alert('Invalid value');
+} else {
+    let num = parseInt(message.replace(/\D+/g,''));
+
+    for (let i = 0; i < message.length;i++){
+        num = message.charAt(i);
+        word.push(num);
+    }
+    if (word.length % half === 0) {
+        alert( word [ word.length / half - 1 ] + word [ word.length / half ] );
+    } else {
+    alert(word[Math.floor(word.length/half)]);
+    }
+}
