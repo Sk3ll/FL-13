fc76ece29e8089a9c668c248dfd88f1dcacbe4f8
Sk3ll <sk3llyar@gmail.com>
2020-05-11 21:37:30 +0300

    final fix

--- FL13_HW11/homework/app.js
+++ FL13_HW11/homework/app.js
@@ -1,40 +1,33 @@
 'use strict';
 
-const data = [
-  {
+const data = [{
     'folder': true,
     'title': 'Pictures',
-    'children': [
-      {
+    'children': [{
         'title': 'logo.png'
       },
       {
         'folder': true,
         'title': 'Vacations',
-        'children': [
-          {
-            'title': 'spain.jpeg'
-          }
-        ]
+        'children': [{
+          'title': 'spain.jpeg'
+        }]
       }
     ]
   },
   {
     'folder': true,
     'title': 'Desktop',
-    'children': [
-      {
-        'folder': true,
-        'title': 'screenshots',
-        'children': null
-      }
-    ]
+    'children': [{
+      'folder': true,
+      'title': 'screenshots',
+      'children': null
+    }]
   },
   {
     'folder': true,
     'title': 'Downloads',
-    'children': [
-      {
+    'children': [{
         'folder': true,
         'title': 'JS',
         'children': null
@@ -60,22 +53,22 @@ function createTree(rootNode, obj) {
 
 function createTreeDom(obj) {
   //if (!obj.length) return;
-  
+
   const ul = document.createElement('ul');
   ul.className = 'tree';
 
   for (let key in obj) {
     const li = document.createElement('li');
-    li.innerHTML = obj[key].title ;
+    li.innerHTML = obj[key].title;
 
     let folders = '';
-    if (obj[key].folder === true){
+    if (obj[key].folder === true) {
       folders = `
       <span id="folder" class="material-icons">
       folder
       </span>
       `;
-    }else if(obj[key].folder === false) {
+    } else if (obj[key].folder === false) {
       folders = `<span id="folder" class="material-icons">
         folder_open
       </span>`;
@@ -84,63 +77,119 @@ function createTreeDom(obj) {
       insert_drive_file
       </span>`
     }
-    
-    
-    
+
+
+
     let span = document.createElement('span');
-    span.className = 'text'
-    span.insertAdjacentHTML('beforeend', folders)
+    span.className = 'text';
+    span.insertAdjacentHTML('afterbegin', folders);
     li.prepend(span);
     span.append(span.nextSibling);
-    
+
+
     span.onclick = (event) => {
       const target = event.target;
-      
-      
-      
-      if(obj[key].folder === false){
-        obj[key].folder = true;
-      } else if(obj[key].folder === true) {
+      const folder = target.querySelector('span');
+      folder.innerHTML = '';
+      let folders = '';
+      if (obj[key].folder === true) {
         obj[key].folder = false;
+        folders = `
+        <span id="folder" class="material-icons">
+        folder
+        </span>
+        `;
+      } else if (obj[key].folder === false) {
+        obj[key].folder = true;
+        folders = `<span id="folder" class="material-icons">
+          folder_open
+        </span>`;
       } else {
         obj[key].folder = undefined;
+        folders = `<span id="file" class="material-icons">
+        insert_drive_file
+        </span>`
       }
+      folder.insertAdjacentHTML('beforeend', folders);
+
 
       if (target.tagName !== 'SPAN') {
         return;
       }
       let childrenContainer = target.parentNode.querySelector('ul');
       if (!childrenContainer) {
-         return; 
+        return;
       }
       childrenContainer.hidden = !childrenContainer.hidden;
+
+
+
+
     }
-    
+
     let childrenUl = createTreeDom(obj[key].children);
     if (childrenUl) {
       li.append(childrenUl);
-    } 
-    if (obj[key].children === null){
-      childrenUl.innerHTML = '<li class="text-null">Folder is empty</li>' ;
     }
-    if (obj[key].folder){
+    if (obj[key].children === null) {
+      childrenUl.innerHTML = '<li class="text-null">Folder is empty</li>';
+    }
+    if (obj[key].folder) {
       obj[key].folder = false;
     }
     childrenUl.hidden = true;
     ul.append(li);
-    
+
   }
+
   return ul;
-  
+
 }
 
+
+function createContextMenu() {
+  const ul = document.createElement('ul');
+  ul.className = 'right-click-menu';
+
+  const li1 = document.createElement('li');
+  const li2 = document.createElement('li');
+  li1.id = 'contentEditable';
+  li2.id = 'delete';
+  li1.textContent = 'Rename';
+  li2.textContent = 'Delete item';
+  ul.append(li1);
+  ul.append(li2);
+  rootNode.append(ul);
+
+
+  let del = '';
+
+  rootNode.addEventListener('contextmenu', event => {
+    event.preventDefault();
+    ul.style.top = `${event.clientY}px`;
+    ul.style.left = `${event.clientX}px`;
+    ul.classList.add('active');
+    del = event.target.parentNode.querySelector('span');
+
+  }, false);
+
+  document.addEventListener('click', event => {
+    const rightClick = 2;
+    if (event.button !== rightClick) {
+      ul.classList.remove('active');
+    }
+  }, false);
+
+  document.querySelector('#contentEditable').addEventListener('click', () => {
+    del.contentEditable = true;
+  }, false);
+  document.querySelector('#delete').addEventListener('click', () => {
+    del.remove();
+  }, false);
+
+
+
+  return ul;
+}
+createContextMenu();
 createTree(rootNode, data);
-
-
-
-rootNode.addEventListener('contextmenu', () => {
-  event.preventDefault();
-  console.log('context')
-})
-
-
